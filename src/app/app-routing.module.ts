f117b9989c0d39f0923e1c0c567fb5486a0d87b7
Naznin Shafeek <naznin567shafeek@gmail.com>
2023-04-03 12:05:35 +0530

    Ustcafeteria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",redirectTo:"register",pathMatch:"full"},
+{path:"register",component:MenuCardComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeRegService } from './menu-card.service';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers:[EmployeRegService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
